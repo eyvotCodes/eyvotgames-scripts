4bc7a3f132722fb72500a2a74e925de8385a71ef
Fabián Leyva <fleyva.home@outlook.com>
2023-04-18 20:43:09 -0600

    importar media itemas al media pool, logger configurado, función para cambio de página

--- editing/editing_step_01.py
+++ editing/editing_step_01.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import datetime
+import json
+import logging
 import psutil
 import time
 import subprocess
@@ -9,16 +11,27 @@ from tqdm import tqdm
 
 
 # parámetros iniciales
-PARAMS_JSON_FILE_PATH = '/Users/fleyva/Desktop/video-script-assets/params.json'
+PARAMS_JSON_FILE_PATH = '/Users/fleyva/projects/@eyvotTest/_youtube/test-assets/params.json'
 BASE_PROJECT_NAME = '00-base (Copy)'
 DAVINCI_RESOLVE_APP_NAME = 'DaVinci Resolve'
 DAVINCI_RESOLVE_PROCESS_NAME = 'DaVinciPanelDaemon'
 SECONDS_TO_WAIT_FOR_DAVINCI_TO_OPEN = 7
 SECONDS_TO_WAIT_FOR_DAVINCI_TO_QUIT = 2
 SECONDS_TO_WAIT_FOR_PROJECT_SAVING = 1
+SECONDS_TO_WAIT_FOR_PAGE_CHANGE = 1
 
 # davinci resolve api values
 RESOLVE_INITIALIZER = 'Resolve'
+PAGE_MEDIA_NAME = 'media'
+PAGE_EDIT_NAME = 'edit'
+PAGE_FUSION_NAME = 'fusion'
+
+# media dirs
+HORIZONTAL_VIDEO_MEDIA_DIR = 'H16.9FHD'
+ASSETS_MEDIA_DIR = 'assets'
+AUDIO_MEDIA_DIR = 'audio'
+IMAGE_MEDIA_DIR = 'image'
+VIDEO_MEDIA_DIR = 'video'
 
 # info messages
 INFO_MESSAGE_LOADING_DAVINCI_RESOLVE = 'Abriendo DaVinci Resolve'
@@ -29,32 +42,15 @@ INFO_MESSAGE_SAVING_PROJECT          = '       Guardando cambios'
 ERROR_MESSAGE_JSON_FILE_NOT_FOUND = 'No se pudo encontrar el archivo JSON.'
 ERROR_MESSAGE_JSON_SYNTAX = 'Error de sintaxis en archivo JSON.'
 ERROR_MESSAGE_BASE_PROJECT_NOT_FOUND = 'No se encontró el proyecto base en DaVinci Resolve.'
+ERROR_MESSAGE_MEDIA_DIR_NOT_FOUND = 'No se encontró el directorio en el Media Pool.'
 
-
-"""
-TODO: Función para crear Estructura
-* Crear estrucrtura de líneas de tiempo necesarias
-  - Bin "H16.9FHD" dentro del root o master del Media Pool
-  - Línea del tiempo "H16.9FHD" delntro del bin recién creado
-  - Línea del tiempo "H16.9FHD - Main" dentro del bin recién creado
-  - Línea del tiempo "H16.9FHD - Canvas" dentro del bin recién creado
-  - Línea del tiempo "H16.9FHD - Content" dentro del bin recién creado
-  - Línea del tiempo "H16.9FHD - Hook" dentro del bin recién creado
-  - Línea del tiempo "H16.9FHD - Intro" dentro del bin recién creado
-  - Línea del tiempo "H16.9FHD - Subject" dentro del bin recién creado
-"""
-
-
-"""
-TODO: Función para realizar los Imports
-* Realizar imports partiendo de un documento JSON de Parámetros
-  - Importar video de gameplay al Media Pool en ./assets/video/
-  - Importar video de grabación al Media Pool en ./assets/video/
-  - Importar video de intro al Media Pool en ./assets/video
-  - Importar video de outro al Media Pool en ./assets/video
-  - Importar audio de grabación al Media Pool en ./assets/audio/
-  - Importar marca de agua al Media Pool en ./assets/image/
-"""
+# logger config
+DEBUG_MODE = True
+if DEBUG_MODE:
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+else:
+    logging.basicConfig(format='%(asctime)s %(message)s')
+logger = logging.getLogger('logger')
 
 
 """
@@ -76,8 +72,6 @@ def get_current_timestamp():
     return timestamp
 
 
-import json
-
 def json_to_dict(json_file_path):
     """
     Carga un diccionario a partir de un archivo json.
@@ -86,16 +80,16 @@ def json_to_dict(json_file_path):
     Returns:
         dict: El diccionario cargado a partir del archivo json.
     Raises:
-        FileNotFoundError: Si la ruta del archivo no existe.
-        json.decoder.JSONDecodeError: Si hay un error en la sintaxis del archivo json.
+        Exception: Si la ruta del archivo no existe.
+        Exception: Si hay un error en la sintaxis del archivo json.
     """
     try:
         with open(json_file_path, 'r') as archivo_json:
             diccionario = json.load(archivo_json)
     except FileNotFoundError:
-        raise Exception(ERROR_MESSAGE_JSON_FILE_NOT_FOUND + '\nDetalles: ' + json_file_path)
+        raise Exception(ERROR_MESSAGE_JSON_FILE_NOT_FOUND + '\n' + json_file_path)
     except json.decoder.JSONDecodeError:
-        raise Exception(ERROR_MESSAGE_JSON_SYNTAX + '\nDetalles: ' + json_file_path)
+        raise Exception(ERROR_MESSAGE_JSON_SYNTAX + '\n' + json_file_path)
     
     return diccionario
 
@@ -122,6 +116,79 @@ def open_davinci_resolve():
     wait_for(SECONDS_TO_WAIT_FOR_DAVINCI_TO_OPEN, INFO_MESSAGE_LOADING_DAVINCI_RESOLVE)
 
 
+def import_to_media_pool_dir(media_paths, media_pool_dir, media_pool_handler):
+    """
+    Importa toda la media compatible dentro de una lista de rutas del disco duro, a un
+    directorio determinado del media pool.
+    Args:
+        media_paths (list): Rutas de los archivos media a importar.
+        media_pool_dir (str): Nombre del directorio destino dentro del media pool.
+        media_pool_handler (obj): Objeto para controlar el media pool del api de davinci resolve.
+    Returns:
+        list: Lista de MediaPoolItems que fueron agregados al media pool.
+    Raises:
+        Exception: Si no existe el directorio dentro del media pool.
+    """
+    root_dir = media_pool_handler.GetRootFolder()
+    root_subdirs = root_dir.GetSubFolderList()
+    horizontal_video_dir = None
+    for subdir in root_subdirs:
+        if subdir.GetName() == HORIZONTAL_VIDEO_MEDIA_DIR:
+            horizontal_video_dir = subdir
+    if not horizontal_video_dir:
+        raise Exception(ERROR_MESSAGE_MEDIA_DIR_NOT_FOUND + '\n' + HORIZONTAL_VIDEO_MEDIA_DIR)
+    horizontal_video_subdirs = horizontal_video_dir.GetSubFolderList()
+    assets_dir = None
+    for subdir in horizontal_video_subdirs:
+        if subdir.GetName() == ASSETS_MEDIA_DIR:
+            assets_dir = subdir
+    if not assets_dir:
+        raise Exception(ERROR_MESSAGE_MEDIA_DIR_NOT_FOUND + '\n' + ASSETS_MEDIA_DIR)
+    assets_subdirs = assets_dir.GetSubFolderList()
+    target_dir = None
+    for subdir in assets_subdirs:
+        if subdir.GetName() == media_pool_dir:
+            target_dir = subdir
+    if not target_dir:
+        raise Exception(ERROR_MESSAGE_MEDIA_DIR_NOT_FOUND + '\n' + media_pool_dir)
+    media_pool_handler.SetCurrentFolder(target_dir)
+    items = media_pool_handler.ImportMedia(media_paths)
+    return items
+
+def switch_to_page(page_name, resolve_handler):
+    """
+    Cambia a una determinada página del flujo de edición de davinci resolve.
+    Args:
+        page_name (str): Nombre de la página a la que se desea cambiar.
+        media_pool_dir (obj): Objeto para controlar davinci resolve del api de davinci resolve.
+    """
+    message = 'Abriendo Página ' + page_name.capitalize()
+    resolve_handler.OpenPage(page_name)
+    wait_for(SECONDS_TO_WAIT_FOR_PAGE_CHANGE, message)
+
+def choose_suffix_to_project_name(name, project_handler):
+    """
+    Determina un sufijo adecuado para el nombre del proyecto.
+    En caso de que se desee poner un proyecto un nombre ocupado por otro proyecto,
+    el sufijo irá cambiando de forma numérica incremental:
+    _01, _02, _03, etc.
+    Args:
+        name (str): Nombre deseado para el proyecto.
+        project_handler (obj): Objeto para controlar el proyecto del api de davinci resolve.
+    Returns:
+        str: Nombre con sufijo adecuado para el proyecto.
+    """
+    i = 1
+    parts = name.split('_')
+    suffix = parts[-1]
+    project_names = project_handler.GetProjectListInCurrentFolder()
+    while name in project_names:
+        i += 1
+        suffix = f'_{i:02d}'
+        name = '_'.join(parts[:-1]) + suffix
+    return name
+
+
 def main():
     # abrir davinci resolve
     open_davinci_resolve()
@@ -134,7 +201,8 @@ def main():
     params = json_to_dict(PARAMS_JSON_FILE_PATH)
     project_name = get_current_timestamp()\
         + '_' + params['gameplay_details']['videogame_keyname']\
-        + '_gameplay'
+        + '_gameplay_01'
+    project_name = choose_suffix_to_project_name(project_name, project_manager)
     project = project_manager.LoadProject(BASE_PROJECT_NAME)
 
     # verificar si existe proyecto de configuración inicial
@@ -142,8 +210,23 @@ def main():
         if params['autoclose']:
             wait_for(SECONDS_TO_WAIT_FOR_DAVINCI_TO_QUIT, INFO_MESSAGE_EXITING_DAVINCI_RESOLVE)
             resolve.Quit()
-        raise Exception(ERROR_MESSAGE_BASE_PROJECT_NOT_FOUND + '\nDetalles: ' + BASE_PROJECT_NAME)
+        raise Exception(ERROR_MESSAGE_BASE_PROJECT_NOT_FOUND + '\n' + BASE_PROJECT_NAME)
     project.SetName(project_name)
+    logger.info(f'Project Name: {project_name}')
+    media_pool = project.GetMediaPool()
+    assets_dirs = params['gameplay_details']['assets']
+
+    # ##################
+    # proceso de edición
+    # ##################
+
+    # importar media
+    switch_to_page(PAGE_EDIT_NAME, resolve)
+    logger.info(f'Media Items to Load: {assets_dirs}')
+    audio_items = import_to_media_pool_dir(assets_dirs['audio'], AUDIO_MEDIA_DIR, media_pool)
+    image_items = import_to_media_pool_dir(assets_dirs['image'], IMAGE_MEDIA_DIR, media_pool)
+    video_items = import_to_media_pool_dir(assets_dirs['video'], VIDEO_MEDIA_DIR, media_pool)
+    logger.info(f'Media Pool Items Loaded: {audio_items}, {image_items}, {video_items}')
 
     # guardar cambios
     project_manager.SaveProject()
